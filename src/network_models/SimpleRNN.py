d07c9f9b026fa15b44412f3973d793dd12e3f3a0
Dominik <ukcqp@student.kit.edu>
2017-12-31 02:12:01 +0100

    SimpleRNN.py should learn correctly now

--- src/network_models/SimpleRNN.py
+++ src/network_models/SimpleRNN.py
@@ -1,42 +1,28 @@
 import torch
-from torch import nn, autograd, optim
+from torch import nn, optim
 from torch.autograd import Variable
-#import torch.nn.functional as F
 
 
 
 class RNN(nn.Module):
     
     
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super().__init__()
         self.hidden_size = hidden_size
-#        self.in_layer = nn.Linear(input_size, hidden_size)
         self.rnn_layer = nn.RNN(input_size=input_size,
                                 hidden_size=hidden_size,
                                 num_layers=num_layers,
 #                                nonlinearity='relu')
                                 nonlinearity='tanh')
-#        self.out_layer = nn.Linear(hidden_size, input_size)
+        self.output_layer = nn.Linear(hidden_size, num_classes)
         self.log_softmax = nn.LogSoftmax()
         
-        
-        #self.embedding = nn.Embedding(input_size, hidden_size)
-        #self.decoder = nn.Linear(hidden_size, input_size)
-        #self.out_layer = nn.Linear(hidden_size[1], num_classes)
-
 
     def forward(self, inp, hidden=None):
-#        output = self.in_layer(inp)
         output, next_hidden = self.rnn_layer(inp, hidden)
+        output = self.output_layer(output)
         output = self.log_softmax(output)
-#        output = self.out_layer(output)
-        
-
-        #x = self.embedding(x)
-        #decoded = s#x = self.embedding(xelf.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
-        #return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
-        
         return output, next_hidden
 
 
@@ -45,45 +31,54 @@ class RNN(nn.Module):
 
 
 
-
-input_size = 3
-hidden_size = 2
-num_layers = 1
-
-model = RNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers)
-criterion = torch.nn.NLLLoss()
-optimizer = optim.Adam(params=model.parameters())
-
-seq_len = 1
-batch_size = 4
-
-torch.manual_seed(99)
-#inp = autograd.Variable(torch.rand(seq_len, batch_size, input_size))
-#inp = autograd.Variable(torch.rand(batch_size, input_size))
-inp = autograd.Variable(torch.randn(seq_len, batch_size, input_size), requires_grad=True)
-hidden = model.initHidden()
-target = autograd.Variable(torch.LongTensor([1, 0, 1, 0]))
-#target = autograd.Variable(torch.rand(batch_size, hidden_size))
-print('INPUT\n', inp)
-print('HIDDEN\n', hidden)
-print('TARGET\n', target)
-print(model)
-
-num_classes = 2
+def main():
+    
+    # parameters
+    input_size = 5
+    hidden_size = 4
+    num_layers = 1
+    seq_len = 1
+    batch_size = 4
+    num_classes = 2
+    num_epochs = 1000
+    num_batches = 1
+    
+    
+    # initialization
+    model = RNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, num_classes=num_classes)
+    criterion = torch.nn.NLLLoss()
+    optimizer = optim.Adam(params=model.parameters())
+    
+#    torch.manual_seed(99)
+#    inp = Variable(torch.randn(seq_len, batch_size, input_size), requires_grad=True)
+    inp = Variable(torch.FloatTensor([[1, 2, 3, 4, 5],
+                                      [5, 4, 3, 2, 1],
+                                      [1, 2, 3, 4, 5],
+                                      [5, 4, 3, 2, 1]]))
+    target = Variable(torch.LongTensor([0, 1, 0, 1]))
+    print('INPUT:\n', inp)
+    print('TARGET:\n', target)
+    print('MODEL:\n', model)
+    
+    
+    # training
+    num_epochs_minus_one = num_epochs - 1
+    num_batches_minus_one = num_batches - 1
+    for epoch in range(num_epochs):
+        hidden = model.initHidden()
+        for batch in range(num_batches):
+            print('Epoch:', epoch, '/', num_epochs_minus_one, '\nBatch:', batch, '/', num_batches_minus_one)
+            output, hidden = model(inp, hidden)
+            output = output.view(batch_size, num_classes)
+            print('OUTPUT:\n', output)
             
-num_epochs = 3
-num_epochs_minus_one = num_epochs - 1
-for epoch in range(num_epochs):
-    print('Epoch:', epoch, '/', num_epochs_minus_one)
-    output, next_hidden = model(inp, hidden)
-#    print('OUTPUT\n', output)
-    output = output.view(batch_size, num_classes)
-    print('VIEW\n', output)
-#    print('NEXT_HIDDEN\n', next_hidden)
-    model.zero_grad()
-    loss = criterion(output, target)
-    loss.backward()
-    optimizer.step()
-
-
-    #loss = F.nll_loss(out, target)
+            model.zero_grad()
+            loss = criterion(output, target)
+            print('LOSS:\n', loss)
+            loss.backward()
+            optimizer.step()
+        
+        
+        
+if __name__ == '__main__':
+    main()
