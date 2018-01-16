6fe7b2660ee443ea6f6688d63694630f29092cc2
TKi <tabea.kiupel@gmx.de>
2018-01-17 00:50:28 +0100

    working version for CUDA RNN training

--- LanguageIdentification/src/Run.sh
+++ LanguageIdentification/src/Run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export PATH="/home/tha/Workplace/code/anaconda2/bin:$PATH" 
-#source activate pytorch-cuda8
-source activate py3-pytorch-cuda8
+source activate pytorch-cuda8
+#source activate py3-pytorch-cuda8
 
 python Main.py
 
